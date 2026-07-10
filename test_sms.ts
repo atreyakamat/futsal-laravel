fa5fc2c54c40928e6057acde87581d6200fe8eea
Atreya Kamat <atkamat1204@gmail.com>
2026-07-10 14:11:44 +0530

    chore: remove test files

--- test_sms.ts
+++
@@ -1,7 +0,0 @@
-import { getSmsProvider } from './lib/sms.ts';
-
-(async () => {
-  const provider = getSmsProvider();
-  await provider.sendSms('7744020601', '123456 is your verification code');
-  await provider.sendSms('7744020601', 'CONFIRMED|2026-07-15|10:00-11:00|TKT-TEST1|REF123|Atreya');
-})();
